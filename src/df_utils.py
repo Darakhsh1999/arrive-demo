329617df48d609b135689ae16c2abf2c357b8c08
darakrash1999 <darakrash@protonmail.com>
2025-06-28 15:08:17 +0200

    added weekday and more util funcs

--- src/df_utils.py
+++ src/df_utils.py
@@ -1,3 +1,4 @@
+import datetime
 import constants
 import pandas as pd
 
@@ -6,3 +7,14 @@ def convert_currency(x: pd.Series) -> pd.Series:
     if x['currency'] != "SEK":
         return x['parking_fee'] * constants.CURRENCY_TO_SEK[x['currency']]
     return x['parking_fee']
+
+def time_diff_convert(x: pd.Series) -> pd.Series:
+    """Calculate the time difference between parking_end and parking_start. With the format date format "YYYY-MM-DD HH:MM:SS" """
+    start_date = datetime.datetime.strptime(x['parking_start_time'], '%Y-%m-%d %H:%M:%S')
+    end_date = datetime.datetime.strptime(x['parking_end_time'], '%Y-%m-%d %H:%M:%S')
+    time_diff = end_date - start_date
+    return time_diff
+
+def get_weekday(x: pd.Series) -> pd.Series:
+    return pd.to_datetime(x['parking_start_time']).dayofweek
+    
